6c2a9dcbe586393716d4a32841fa6200b475a554
Pitonico <dio_dgba@hotmail.com>
2026-07-12 18:23:59 -0300

    criando os serviços

---
+++ src/main/java/com/diogosson/posto_combustivel/service/TipoCombustivelService.java
@@ -0,0 +1,45 @@
+package com.diogosson.posto_combustivel.service;
+
+import com.diogosson.posto_combustivel.infrastructure.entities.TipoCombustivel;
+import com.diogosson.posto_combustivel.infrastructure.repositories.TipoCombustivelRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TipoCombustivelService {
+
+    private final TipoCombustivelRepository tipoCombustivelRepository;
+
+    public void criar(TipoCombustivel tipo) {
+        tipoCombustivelRepository.save(tipo);
+    }
+
+    public TipoCombustivel buscarPorId(Integer id) {
+        return tipoCombustivelRepository
+                    .findById(id)
+                    .orElseThrow(() ->
+                            new NullPointerException("Tipo de combustivel não encontrado pelo ID " + id));
+    }
+
+    @Transactional
+    private void deletarPorId(Integer id) {
+
+        boolean existeTipo = tipoCombustivelRepository.existsById(id);
+
+        if (!existeTipo) {
+            throw new NullPointerException("Tipo de combustivel não encontrado pelo ID " + id);
+        }
+
+        tipoCombustivelRepository.deleteById(id);
+    }
+
+    private void alterarPorId(Integer id, TipoCombustivel tipo) {
+
+        TipoCombustivel tipoEncontrado = buscarPorId(id);
+        tipo.setId(tipoEncontrado.getId());
+
+        tipoCombustivelRepository.save(tipo);
+    }
+}
